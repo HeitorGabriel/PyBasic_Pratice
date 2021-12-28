a788ede5b1c359fa0d9fe7390834eb3f27307a61
Heitor Gabriel Monteiro <45849458+HeitorGabriel@users.noreply.github.com>
2021-12-28 20:56:38 -0300

    Add files via upload

---
+++ Hect_Pratc.py
@@ -0,0 +1,17 @@
+"""
+Heitor Gabriel, the author:
+   https://github.com/HeitorGabriel
+Pratice Excercise with Pokemon Data:
+   https://www.kaggle.com/rounakbanik/pokemon
+
+# ! >>>> On working: unfinished project.  <<<< ! #
+
+"""
+
+import os, sys             # interact with system
+import numpy       as np   # manipulation
+import pandas      as pd   # manipulation
+import statsmodels as stm  # statistics
+import seaborn     as sb   # visualization
+
+os.chdir('/home/heitor/ProjetosPy/ML_PyPratice/Basic')
